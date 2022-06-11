c7bd573b0a4d9d346d0c0ad568227ab72db196e8
Alexandr Kirpichyov <alexandr.kirpichyov@gmail.com>
2022-06-11 19:12:57 +0300

    Beautify Program.cs configuration

---
+++ src/LightBoard.Api/Configurators/MiddlewareConfigurator.cs
@@ -0,0 +1,41 @@
+﻿using LightBoard.Api.Extensions;
+using LightBoard.Application.Abstractions.Options;
+using Serilog;
+
+namespace LightBoard.Api.Configurators;
+
+public static class MiddlewareConfigurator
+{
+    public static void Apply(WebApplication webApplication)
+    {
+        if (webApplication.Environment.IsDevelopment())
+        {
+            webApplication.UseCors(policyBuilder =>
+            {
+                policyBuilder.AllowAnyOrigin();
+                policyBuilder.AllowAnyHeader();
+                policyBuilder.AllowAnyMethod();
+            });
+        
+            webApplication.UseSwagger();
+            webApplication.UseSwaggerUI();
+        }
+
+        webApplication.UseCors(corsPolicyBuilder =>
+        {
+            AuthOptions identityOptions = webApplication.Configuration.BindFromAppSettings<AuthOptions>();
+
+            corsPolicyBuilder.AllowAnyHeader();
+            corsPolicyBuilder.AllowAnyMethod();
+            corsPolicyBuilder.WithOrigins(identityOptions.AllowedCorsList ?? Array.Empty<string>());
+        });
+    
+        webApplication.UseSerilogRequestLogging();
+        webApplication.UseHttpsRedirection();
+
+        webApplication.UseAuthentication();
+        webApplication.UseAuthorization();
+
+        webApplication.MapControllers();
+    }
+}
--- src/LightBoard.Api/Program.cs
+++ src/LightBoard.Api/Program.cs
@@ -1,20 +1,6 @@
-using FluentValidation.AspNetCore;
-using LightBoard.Api.Extensions;
-using LightBoard.Api.Middleware.Exceptions;
-using LightBoard.Api.Middleware.Filters;
-using LightBoard.Api.Middleware.SessionKey;
-using LightBoard.Api.Swagger;
-using LightBoard.Api.Validators;
-using LightBoard.Application.Abstractions.Options;
-using LightBoard.Shared.Api;
-using Microsoft.OpenApi.Models;
-using Newtonsoft.Json.Converters;
-using Newtonsoft.Json.Serialization;
+using LightBoard.Api.Configurators;
 using Serilog;
-using Serilog.Events;
-using Serilog.Sinks.SystemConsole.Themes;
 
-// ----------- Application builder -----------
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseDefaultServiceProvider((_, options) =>
@@ -24,10 +10,10 @@ builder.Host.UseDefaultServiceProvider((_, options) =>
             })
             .UseSerilog();
 
-ConfigureSerilog();
-ConfigureServices(builder.Services);
+SerilogConfigurator.Apply(builder.Configuration);
+ServicesConfigurator.Apply(builder.Services, builder.Configuration, builder.Environment);
 WebApplication app = builder.Build();
-ConfigurePipeline(app);
+MiddlewareConfigurator.Apply(app);
 
 var startupLogger = Log.ForContext<Program>();
 
@@ -43,114 +29,4 @@ catch (Exception exception)
 finally
 {
     Log.CloseAndFlush();
-}
-
-// ----------- Configuration -----------
-void ConfigureServices(IServiceCollection services)
-{
-    services.AddHttpContextAccessor();
-    
-    services.AddPostgreSqlContext(builder.Configuration, builder.Environment)
-            .AddRedisContext(builder.Configuration)
-            .AddApplicationServices()
-            .AddApplicationOptions(builder.Configuration)
-            .AddSessionKeyAuthorization();
-
-    services.AddRouting(options => options.LowercaseUrls = true);
-    
-    services.AddControllers()
-            .AddNewtonsoftJson(options =>
-            {
-                options.SerializerSettings.Converters.Add(
-                    new StringEnumConverter(new CamelCaseNamingStrategy())
-                );
-            })
-            .AddFluentValidation(configuration =>
-            {
-                configuration.RegisterValidatorsFromAssemblyContaining<RegisterRequestValidator>();
-            })
-            .AddMvcOptions(options =>
-            {
-                options.Filters.Add<ExceptionFilter>();
-                options.Filters.Add<FluentValidationFilter>();
-            })
-            .ConfigureApiBehaviorOptions(options => options.SuppressModelStateInvalidFilter = true);;
-
-    services.AddEndpointsApiExplorer();
-    services.AddSwaggerGenNewtonsoftSupport()
-            .AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Version = "v1",
-                    Title = "AssistEasy API"
-                });
-                            
-                options.AddSecurityDefinition("SessionKey", new OpenApiSecurityScheme
-                {
-                    Name = ApiHeaders.SessionKey,
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Basic",
-                    In = ParameterLocation.Header,
-                    Description = "Obtained unique SessionKey."
-                });
-                            
-                options.OperationFilter<AuthOperationFilter>();
-            });
-    
-}
-
-void ConfigurePipeline(WebApplication webApplication)
-{
-    if (webApplication.Environment.IsDevelopment())
-    {
-        app.UseCors(policyBuilder =>
-        {
-            policyBuilder.AllowAnyOrigin();
-            policyBuilder.AllowAnyHeader();
-            policyBuilder.AllowAnyMethod();
-        });
-        
-        webApplication.UseSwagger();
-        webApplication.UseSwaggerUI();
-    }
-
-    app.UseCors(corsPolicyBuilder =>
-    {
-        AuthOptions identityOptions = webApplication.Configuration.BindFromAppSettings<AuthOptions>();
-
-        corsPolicyBuilder.AllowAnyHeader();
-        corsPolicyBuilder.AllowAnyMethod();
-        corsPolicyBuilder.WithOrigins(identityOptions.AllowedCorsList ?? Array.Empty<string>());
-    });
-    
-    webApplication.UseSerilogRequestLogging();
-    webApplication.UseHttpsRedirection();
-
-    webApplication.UseAuthentication();
-    webApplication.UseAuthorization();
-
-    webApplication.MapControllers();
-}
-
-void ConfigureSerilog()
-{
-    const string consoleOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} <s:{SourceContext}>{NewLine}{Exception}";
-    const string fileOutputTemplate = "{Timestamp:G} {Message}{NewLine:1}{Exception:1}";
-    const string logsPath = "Logs/log-.txt";
-
-    if (!Enum.TryParse(builder.Configuration["Serilog:MinimumLevel"], out LogEventLevel minimalLevel))
-    {
-        throw new ArgumentException($"Serilog logging level value '{minimalLevel}' is invalid");
-    }
-
-    Log.Logger = new LoggerConfiguration()
-        .MinimumLevel.Is(minimalLevel)
-        .MinimumLevel.Override("Microsoft", minimalLevel)
-        .Enrich.FromLogContext()
-        .Enrich.WithProcessId()
-        .Enrich.WithThreadId()
-        .WriteTo.Console(theme: SystemConsoleTheme.Literate, outputTemplate: consoleOutputTemplate)
-        .WriteTo.File(path: logsPath, outputTemplate: fileOutputTemplate, rollingInterval: RollingInterval.Day)
-        .CreateLogger();
 }
