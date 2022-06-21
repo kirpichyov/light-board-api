f1fabe23f02db8fc60e730195935db3d79e69e68
illyxancuh <illya23082001@ukr.net>
2022-06-21 18:07:26 +0300

    research and implement default user avatars

---
+++ src/LightBoard.Shared/Exceptions/ExternalApiException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightBoard.Shared.Exceptions
+{
+    public class ExternalApiException : ExceptionBase
+    {
+        public string ApiResponse { get; }
+
+        public ExternalApiException(string message, string apiResponse)
+            : base(message, ExceptionIdentifiers.Generic)
+        {
+            ApiResponse = apiResponse;
+        }
+    }
+}
