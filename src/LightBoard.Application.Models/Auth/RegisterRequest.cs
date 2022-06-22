45aec8b3d58e30a6e5cbfd9b84b2a0fe1047ec19
Alexandr Kirpichyov <alexandr.kirpichyov@gmail.com>
2022-06-23 00:44:52 +0300

    Extend user with surname

--- src/LightBoard.Application.Models/Auth/RegisterRequest.cs
+++ src/LightBoard.Application.Models/Auth/RegisterRequest.cs
@@ -6,5 +6,6 @@ public class RegisterRequest : IContainsPassword
 {
     public string Email { get; set; }
     public string Name { get; set; }
+    public string Surname { get; set; }
     public string Password { get; set; }
 }
--- src/LightBoard.Application.Models/Cards/AssigneeResponse.cs
+++ src/LightBoard.Application.Models/Cards/AssigneeResponse.cs
@@ -3,6 +3,6 @@
 public class AssigneeResponse
 {
     public Guid Id { get; set; }
-    public string Name { get; set; }
+    public string FullName { get; set; }
     public Guid UserId { get; set; }
 }
